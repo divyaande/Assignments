11074aebe5b03d456b2ebd368b66d76db184a84c
divyaande <48804665+divyaande@users.noreply.github.com>
2025-05-12 20:53:48 +0530

    Update Assignment1.java

--- src/assignments/Assignment1.java
+++ src/assignments/Assignment1.java
@@ -5,6 +5,36 @@ public class Assignment1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc=new Scanner(System.in);
+    System.out.println("enter the string:");
+    
+    
+    String str=sc.next();
+    String org_str=str;
+    
+    String rev="";
+    
+	int len=str.length();
+	
+	for(int i=len-1;i>=0;i--)
+	{
+		rev=rev+str.charAt(i);
+	}
+	if(org_str.equals(rev))
+			{
+		    System.out.println(org_str+" is palindrome string");
+		    
+			}
+	else
+	{
+		 System.out.println(org_str+" is palindrome string");
+	}
+		
+	
+}
+}
+
+
 	}
 
 }
